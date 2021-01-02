af87ebc5554465c0f2d7935a7a5ba7312ea86a4b
miladsh52 <miladsh52@gmail.com>
2021-01-02 19:12:28 +0330

    Update keygen.c

--- enclave/keygen.c
+++ enclave/keygen.c
@@ -40,11 +40,81 @@ sgx_status_t ecall_key_gen_and_seal(char *pubkey, size_t pubkey_size, char *seal
 
   // Step 2: Create Key Pair.
   sgx_ec256_private_t p_private;
-  if ((ret = sgx_ecc256_create_key_pair(&p_private, (sgx_ec256_public_t *)pubkey, p_ecc_handle)) != SGX_SUCCESS)
+  // if ((ret = sgx_ecc256_create_key_pair(&p_private, (sgx_ec256_public_t *)pubkey, p_ecc_handle)) != SGX_SUCCESS)
+  // {
+  //   print("\nTrustedApp: sgx_ecc256_create_key_pair() failed !\n");
+  //   goto cleanup;
+  // }
+
+  for(int i=0;i<SGX_ECP256_KEY_SIZE;i++)
   {
-    print("\nTrustedApp: sgx_ecc256_create_key_pair() failed !\n");
+    p_private.r[i] = 0x02;
+  }
+
+  if ((ret = sgx_ecc256_calculate_pub_from_priv(&p_private, (sgx_ec256_public_t *)pubkey)) != SGX_SUCCESS)
+  {
+    print("\nTrustedApp: sgx_ecc256_calculate_pub_from_priv() failed !\n");
     goto cleanup;
   }
+
+  static const uint8_t  kQx[] = {
+    0xc8, 0x15, 0x61, 0xec, 0xf2, 0xe5, 0x4e, 0xde, 0xfe, 0x66, 0x17,
+    0xdb, 0x1c, 0x7a, 0x34, 0xa7, 0x07, 0x44, 0xdd, 0xb2, 0x61, 0xf2,
+    0x69, 0xb8, 0x3d, 0xac, 0xfc, 0xd2, 0xad, 0xe5, 0xa6, 0x81
+  };
+  static const uint8_t  kQy[] = {
+    0xe0, 0xe2, 0xaf, 0xa3, 0xf9, 0xb6, 0xab, 0xe4, 0xc6, 0x98, 0xef,
+    0x64, 0x95, 0xf1, 0xbe, 0x49, 0xa3, 0x19, 0x6c, 0x50, 0x56, 0xac,
+    0xb3, 0x76, 0x3f, 0xe4, 0x50, 0x7e, 0xec, 0x59, 0x6e, 0x88
+  };
+  static const uint8_t  kD[] = {
+    0xc6, 0xc1, 0xaa, 0xda, 0x15, 0xb0, 0x76, 0x61, 0xf8, 0x14, 0x2c,
+    0x6c, 0xaf, 0x0f, 0xdb, 0x24, 0x1a, 0xff, 0x2e, 0xfe, 0x46, 0xc0,
+    0x93, 0x8b, 0x74, 0xf2, 0xbc, 0xc5, 0x30, 0x52, 0xb0, 0x77
+  };
+
+  for(int i=0;i<SGX_ECP256_KEY_SIZE;i++)
+  {
+    p_private.r[i] = kD[i];
+  }
+
+  for(int i=0;i<SGX_ECP256_KEY_SIZE;i++)
+  {
+    pubkey[i] = kQx[SGX_ECP256_KEY_SIZE-1-i];
+  }
+  for(int i=0;i<SGX_ECP256_KEY_SIZE;i++)
+  {
+    pubkey[i+32] = kQy[SGX_ECP256_KEY_SIZE-1-i];
+  }
+
+  print("\n\n\n");
+
+  for(int i=0;i<SGX_ECP256_KEY_SIZE;i++)
+  {
+    printInt(p_private.r[i]);
+  }
+
+  // print("\n");
+
+  // for(int i=SGX_ECP256_KEY_SIZE-1; i>=0;i--)
+  // {
+  //   printInt(p_private.r[i]);
+  // }
+
+  print("\n\n\n");
+  for(int i=0;i<SGX_ECP256_KEY_SIZE*2;i++)
+  {
+    printInt((uint8_t)pubkey[i]);
+  }
+  
+  // print("\n");
+
+  // for(int i=SGX_ECP256_KEY_SIZE*2-1;i>=0;i--)
+  // {
+  //   printInt((uint8_t)pubkey[i]);
+  // }
+
+  print("\n\n\n");
 
   // Step 3: Calculate sealed data size.
   if (sealedprivkey_size >= sgx_calc_sealed_data_size(0U, sizeof(p_private)))
